038e9a0a1a9aee9196e527ef1159443bb2de05ff
xyhuang <xiaoyuh98@gmail.com>
2025-11-25 20:55:43 +0100

    add all test

--- tests/python/test_pbm3c2.py
+++ tests/python/test_pbm3c2.py
@@ -160,19 +160,19 @@ def test_knockout_stage_1_read_segment_ids(epochs_segmented):
 
 #     print("-> STAGE 5 PASSED (in Python)")
 
-# def test_compute_distances(epochs_segmented, pbm3c2_correspondences_file):
-#     epoch0, epoch1 = epochs_segmented
-#     correspondences_file = pbm3c2_correspondences_file
-#     apply_ids = np.arange(1, 31)
+def test_compute_distances(epochs_segmented, pbm3c2_correspondences_file):
+    epoch0, epoch1 = epochs_segmented
+    correspondences_file = pbm3c2_correspondences_file
+    apply_ids = np.arange(1, 31)
 
-#     alg = py4dgeo.PBM3C2(registration_error=0.01)
+    alg = py4dgeo.PBM3C2(registration_error=0.01)
 
-#     rez = alg.run(
-#         epoch0=epoch0,
-#         epoch1=epoch1,
-#         correspondences_file=correspondences_file,
-#         apply_ids=apply_ids,
-#         search_radius=5.0,
-#     )
+    rez = alg.run(
+        epoch0=epoch0,
+        epoch1=epoch1,
+        correspondences_file=correspondences_file,
+        apply_ids=apply_ids,
+        search_radius=5.0,
+    )
 
-#     assert rez is not None
+    assert rez is not None
