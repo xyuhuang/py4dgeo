d8606361662539aa65df3f94d5b0dacb9016ebe5
xyhuang <xiaoyuh98@gmail.com>
2025-11-25 14:19:29 +0100

    remove preprocess test

--- tests/python/test_pbm3c2.py
+++ tests/python/test_pbm3c2.py
@@ -33,19 +33,19 @@ def cleanup_after_test():
                 pass
 
 
-def test_preprocess(epochs_segmented,pbm3c2_correspondences_file):
-    epoch0, epoch1 = epochs_segmented
-    correspondences_file = pbm3c2_correspondences_file
+# def test_preprocess(epochs_segmented,pbm3c2_correspondences_file):
+#     epoch0, epoch1 = epochs_segmented
+#     correspondences_file = pbm3c2_correspondences_file
 
-    alg = py4dgeo.PBM3C2(registration_error=0.01)
-    epoch0_preprocessed, epoch1_preprocessed, correspondences_df =alg.preprocess_epochs(epoch0, epoch1, correspondences_file)
+#     alg = py4dgeo.PBM3C2(registration_error=0.01)
+#     epoch0_preprocessed, epoch1_preprocessed, correspondences_df =alg.preprocess_epochs(epoch0, epoch1, correspondences_file)
 
-    assert epoch0_preprocessed is not None
-    assert epoch1_preprocessed is not None
-    assert correspondences_df is not None
+#     assert epoch0_preprocessed is not None
+#     assert epoch1_preprocessed is not None
+#     assert correspondences_df is not None
 
-    assert epoch0_preprocessed.cloud.shape[0] > 0, "Preprocessed epoch0 should have points"
-    assert epoch1_preprocessed.cloud.shape[0] > 0, "Preprocessed epoch1 should have points"
+#     assert epoch0_preprocessed.cloud.shape[0] > 0, "Preprocessed epoch0 should have points"
+#     assert epoch1_preprocessed.cloud.shape[0] > 0, "Preprocessed epoch1 should have points"
 
 
 # def test_compute_distances(epochs_segmented, pbm3c2_correspondences_file):
